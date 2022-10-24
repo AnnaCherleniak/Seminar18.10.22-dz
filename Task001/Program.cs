f704d21591fd80def75df9f478001c2e4910021c
Anna <ankakras@yandex.ru>
2022-10-24 17:20:19 +0700

    Решение

---
+++ Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿// 1. Найти точку пересечения двух прямых заданных уравнением
+// y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine("Введите коэффициент k1 - ");
+int k1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите коэффициент b1 - ");
+int b1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите коэффициент k2 - ");
+int k2 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите коэффициент b2 - ");
+int b2 = int.Parse(Console.ReadLine() ?? "0");
+if(k1 == k2 && b1 == b2)
+    Console.WriteLine("Прямые совпадают");
+else if(k1==k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    int x = (b1-b2)/(k2-k1);
+    int y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения прямых - ({x},{y})");
+}
