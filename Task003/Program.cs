6f0f32d2a4503c958370703fce0c5e70d1780c92
Anna <ankakras@yandex.ru>
2022-10-25 17:53:45 +0700

    Решение

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,39 +1,25 @@
 ﻿// 3. Написать программу масштабирования фигуры
-
-void PrintImage(int[,]image)
+void ResizeFigure(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int k)
 {
-    for(int i = 0; i < image.GetLength(0); i++)
-    {
-        for(int j = 0; j < image.GetLength(1); j++)
-        {
-            if(image[i, j] == 0)
-                Console.Write($" ");
-            else
-                Console.Write($"+");
-        }
-        Console.WriteLine();
-    }
-}
-void ResizeImage(int[,]image, int k)
-{
-    
-        
+    x1 *= k;
+    y1 *= k;
+    x2 *= k;
+    y2 *= k;
+    x3 *= k;
+    y3 *= k;
+    x4 *= k;
+    y4 *= k;
+    Console.WriteLine($"При k = {k} получаем координаты ({x1},{y1})({x2},{y2})({x3},{y3})({x4},{y4})");
 }
 
-void FillImage(int x, int y)
-{
-    image[0,0] = 1;
-}
-
-int[,]image = new int[3,3];
-
-//PrintImage(image);
-//Console.WriteLine("Масштабирование - ");
-image[0,0] = 1;
-image[2,0] = 1;
-image[2,2] = 1;
-image[0,2] = 1;
-Console.WriteLine();
-//ResizeImage(0, 0, 4);
-FillImage(0,0);
-PrintImage(image);
+int x1 = 0;
+int y1 = 0;
+int x2 = 2;
+int y2 = 0;
+int x3 = 2;
+int y3 = 2;
+int x4 = 0;
+int y4 = 2;
+Console.WriteLine("Введите коэффициент масштабированик: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+ResizeFigure(x1, y1, x2, y2, x3, y3, x4, y4, k);
