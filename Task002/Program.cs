57f54bbd29b5151c86b5155bc702230183813b8c
Anna <ankakras@yandex.ru>
2022-11-04 21:26:24 +0700

    Решение

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,13 +1,27 @@
 ﻿// 2. Показать числа Фибоначчи
 
-double Fibonacci(int n)
-{
-    if(n == 1 || n == 2)
-        return 1;
-    else 
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
+//double Fibonacci(int n)
+//{
+  //  if(n == 1 || n == 2)
+    //    return 1;
+    //else 
+        //return Fibonacci(n - 1) + Fibonacci(n - 2);
+//}
 Console.WriteLine("До какого числа вывести таблицу чисел Фибоначчи: ");
 int N = int.Parse(Console.ReadLine() ?? "0");
+int f;
+int f1 = 1;
+int f2 = 1;
+
 for(int i = 1; i <= N; i++)
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+{
+    if(i <= 2)
+        f = 1;
+    else
+    {
+        f = f1 + f2;
+        f2 = f1;
+        f1 = f;
+    }
+    Console.WriteLine($"f({i}) = {f}");
+}
