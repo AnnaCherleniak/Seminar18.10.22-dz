0a315f53b374487dead613a258a713fe2b283759
Anna <ankakras@yandex.ru>
2022-11-08 15:43:46 +0700

    Решение

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,38 +1,56 @@
 ﻿// 3. Написать программу масштабирования фигуры (0,0)(2,0)(2,2)(0,2)
 
-string Replase(string text, char oldValue, char newValue)
+int CountCoordinates(string text, char c)
 {
-    string result = String.Empty;
-    int length = text.Length;
-    for(int i = 0; i < length; i++)
+    int count = 0;
+    for(int i = 0; i < text.Length; i ++)
     {
-        if(text[i] == oldValue)
+        if(text[i] == ',')
+            count++;
+    }
+    return count;
+}
+void ResizeFigure(int[] array, int k)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = array[i] * k;
+}
+string text = "(0,0)(2,0)(2,2)(0,2)";
+int length = CountCoordinates(text, ',');
+int[] x = new int[length];
+int[] y = new int[length];
+int i = 0;
+int j = 0;
+int k = 0;
+while(i < text.Length)
+{
+    if(text[i] == '(')
+    {       
+        if(j < length)
         {
-            result = result + $"{newValue}";
-        }
-        else
-        {
-            result = result + $"{text[i]}";
+            var pointX = $"{text[i+1]}";
+            int temp = int.Parse(pointX);
+            x[j] = temp;
+            j++;
         }
     }
-    return result;
+    if(text[i] == ',')
+    {
+        if(k < length)
+        {
+            var pointX = $"{text[i+1]}";
+            int temp = int.Parse(pointX);
+            y[k] = temp;
+            k++;
+        }
+    }
+    i++;
 }
+Console.WriteLine("Введите коэффициент масштабированик: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+ResizeFigure(x, n);
+ResizeFigure(y, n);
+for(i = 0; i < x.Length; i++)
+    Console.Write($"({x[i]},{y[i]}) ");
+Console.WriteLine();
 
-string text = "(10,0)(12,0)(12,2)(10,2)";
-string points = Replase(text, '(', ' ');
-points = Replase(points, ')', ' ');
-points = Replase(points, ',', ' ');
-Console.WriteLine(points);
-var pointX = $"{points[1]}" + $"{points[2]}";
-int x = int.Parse(pointX);
-Console.WriteLine(pointX);
-Console.WriteLine(x);
-
-
-
-
-
-
-
-//Console.WriteLine("Введите коэффициент масштабированик: ");
-//int k = int.Parse(Console.ReadLine() ?? "0");
