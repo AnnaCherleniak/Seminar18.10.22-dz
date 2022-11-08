fade7c2052275812c31ec8296a2021566d97d5eb
Anna <ankakras@yandex.ru>
2022-11-08 16:28:34 +0700

    Решение

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -13,8 +13,9 @@ void ResizeFigure(int[] array, int k)
     for(int i = 0; i < array.Length; i++)
         array[i] = array[i] * k;
 }
-string text = "(0,0)(2,0)(2,2)(0,2)";
+string text = "(10,20)(12,20)(12,22)(10,22)";
 int length = CountCoordinates(text, ',');
+string t = String.Empty;
 int[] x = new int[length];
 int[] y = new int[length];
 int j = 0;
@@ -22,21 +23,33 @@ int k = 0;
 for(int i = 0; i < text.Length; i++)
 {
     if(text[i] == '(')
-    {       
+    {
+        i++;
         if(j < length)
         {
-            var pointX = $"{text[i+1]}";
-            int temp = int.Parse(pointX);
+            var point = t;
+            while(text[i] != ',')
+            {
+                point += $"{text[i]}";
+                i++;
+            }
+            int temp = int.Parse(point);
             x[j] = temp;
             j++;
         }
     }
     if(text[i] == ',')
     {
+        i++;
         if(k < length)
         {
-            var pointX = $"{text[i+1]}";
-            int temp = int.Parse(pointX);
+            var point = t;
+            while(text[i] != ')')
+            {
+                point += $"{text[i]}";
+                i++;
+            }
+            int temp = int.Parse(point);
             y[k] = temp;
             k++;
         }
