a7fbedb2e5407f8a0e08c571731b97dda4055634
Anna <ankakras@yandex.ru>
2022-11-08 11:54:54 +0700

    Решение

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,18 +1,34 @@
 ﻿// 3. Написать программу масштабирования фигуры (0,0)(2,0)(2,2)(0,2)
 
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue)
+        {
+            result = result + $"{newValue}";
+        }
+        else
+        {
+            result = result + $"{text[i]}";
+        }
+    }
+    return result;
+}
+
+string text = "(10,0)(12,0)(12,2)(10,2)";
+string points = Replase(text, '(', ' ');
+points = Replase(points, ')', ' ');
+points = Replase(points, ',', ' ');
+Console.WriteLine(points);
 
 
-Console.WriteLine("Введите координаты вершин фигуры списком: ");
-string point =  String.Empty;
-point = Convert.ToString(Console.ReadLine() ?? "0");
-//int[] x = new int[4];
-//int[] y = new int[4];
-int x1 = Convert.ToInt32(point[6]);
-//PrintArray(point);
-Console.WriteLine(x1);
+
+
 
 
 
 //Console.WriteLine("Введите коэффициент масштабированик: ");
 //int k = int.Parse(Console.ReadLine() ?? "0");
-///ResizeFigure(x1, y1, x2, y2, x3, y3, x4, y4, k);
